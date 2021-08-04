8ff1f26ac9fa3ec269eb7d9cc78a2475f3983e38
Nawed Anjum <nawedanjum1111@gmail.com>
2021-08-05 00:37:20 +0530

    fixed auth test

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -8,8 +8,7 @@ it('responds with details about the current user', async () => {
     .set('Cookie', cookie)
     .send()
     .expect(200);
-
-  expect(response.body.email).toEqual('test@test.com');
+  expect(response.body.user.email).toEqual('test@test.com');
 });
 
 it('responds with unauthorized if not authenticated', async () => {
